c14aedfac5fd06a0ce53d9951626dc9278196161
TETIANA SUKHORSKA <Suherichtata@gmail.com>
2025-09-17 00:51:27 +0300

    add 1,2 tasks

---
+++ js/task-1.js
@@ -0,0 +1,13 @@
+const categoriesList = document.querySelector("#categories");
+
+const categoriesItems = categoriesList.querySelectorAll(".item");
+console.log("Number of categories:", categoriesItems.length);
+
+categoriesItems.forEach((item) => {
+  const categoryTitle = item.querySelector("h2").textContent;
+
+  console.log("Category:", categoryTitle);
+
+  const categiryEl = item.querySelectorAll("li").length;
+  console.log("Elements:", categiryEl);
+});
