14c07bbc4c6fb9538abac83e06b817ca0715a1c1
TETIANA SUKHORSKA <Suherichtata@gmail.com>
2025-09-19 19:44:23 +0300

    add 3-5 tasks

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector("form");
+loginForm.addEventListener("submit", handleSubmit);
 
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const login = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+
+  if (login === "" || password === "") {
+    alert("All form fields must be filled in");
+  } else {
+    const formData = {
+      email: login,
+      password: password,
+    };
+    console.log(formData);
+
+    form.reset();
+  }
+}
--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,14 @@
+const body = document.querySelector("body");
+const button = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+button.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  span.textContent = randomColor;
+});
